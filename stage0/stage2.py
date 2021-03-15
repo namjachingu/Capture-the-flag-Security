42098c84039ecefe4b7ddd8ba8753005ac1dd8d0
Felicia <56272326+namjachingu@users.noreply.github.com>
2021-03-15 11:39:02 +0100

    Update stage2.py

--- stage0/stage2.py
+++ stage0/stage2.py
@@ -18,31 +18,13 @@ amp_host = env.AMP.get("host")
 amp_client_id = env.AMP.get("client_id")
 amp_api_key = env.AMP.get("api_key")
 
-resp_amp = requests.get(f"https://{amp_client_id}:{amp_api_key}@{amp_host}/v1/event_types")
-#resp_amp = requests.get(f"https://{amp_client_id}:{amp_api_key}@{amp_host}/v1/events")
+#resp_amp = requests.get(f"https://{amp_client_id}:{amp_api_key}@{amp_host}/v1/event_types")
 #print(resp_amp.json())
-'''
-#for org in resp_amp.json()["metadata"]["results"]:
-for org in resp_amp.json()["data"]:
-    if org["name"] == "Executed malware":
-        print(org["id"])
-        print(org["description"])
-        x = org["id"]
-'''
+
 resp_amp = requests.get(f"https://{amp_client_id}:{amp_api_key}@{amp_host}/v1/events")
-print(resp_amp.json())
+#print(resp_amp.json())
 
 for org in resp_amp.json()["data"]:
-    print(org)
-    if org["hostname"] == "Demo_AMP_Threat_Audit":
-        print(org["hostname"])
-
-
-
-
-#url = "https://api.amp.cisco.com/v1/events?limit=2"
-#url = resp_amp
-#headers =  {"Content-Type": "application/json", "accept": "application/json"}
-#response = requests.get(url, headers=headers)
-#print(response.json())
+    if org["computer"]["hostname"] == "Demo_AMP_Threat_Audit":
+        print(org["computer"]["hostname"])
 
